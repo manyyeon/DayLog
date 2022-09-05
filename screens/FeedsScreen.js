0de7a84a568d5fefcd289f59fdf6edc7901146c4
manyyeon <udayeon@naver.com>
2022-09-05 15:20:19 +0900

    Context API

--- screens/FeedsScreen.js
+++ screens/FeedsScreen.js
@@ -1,17 +1,29 @@
 import React from 'react';
+import {useContext} from 'react';
+import {TextInput} from 'react-native';
 import {StyleSheet, View, Text} from 'react-native';
 import LogContext from '../contexts/LogContext';
 
 const FeedsScreen = () => {
+  const {text, setText} = useContext(LogContext);
   return (
     <View style={styles.block}>
-      <LogContext.Consumer>{value => <Text>{value}</Text>}</LogContext.Consumer>
+      <TextInput
+        value={text}
+        onChangeText={setText}
+        placeholder="텍스트를 입력하세요"
+        style={styles.input}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   block: {},
+  input: {
+    padding: 16,
+    backgroundColor: 'white',
+  },
 });
 
 export default FeedsScreen;
